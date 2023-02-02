71a88c8fcd0ca0a9fe74eddc7fbffeb2520bdd8c
ababey <git@ababey.com>
2023-02-02 10:48:59 +0100

    Setuplayout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,26 @@
-import React, { Children } from 'react';
+import React, { Children } from "react";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import CssBaseline from "@mui/material/CssBaseline";
 
-type Props  = {
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import CustomAppBar from "./navbar";
+
+type Props = {
   children: React.ReactNode;
-}
+};
 
-const Layout = ({children}: Props) => {
-  return <div>
-    {children}
-  </div>
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <CssBaseline />
+      <CustomAppBar />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
